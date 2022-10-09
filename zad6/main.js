56019ffb994312bdab9cf08609d31ef45239e2bc
oliwiajarguz <112726036+oliwiajarguz@users.noreply.github.com>
2022-10-09 23:21:49 +0200

    Add files via upload

---
+++ zad6/main.js
@@ -0,0 +1,14 @@
+var jsonPracownicy = { 
+    "pracownicy":[
+        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+        {"firstName": "Anna", "lastName": "Szapiel"},
+        {"firstName": "Piotr", "lastName": "Żmuda"}
+    ]
+};
+console.log(jsonPracownicy.pracownicy[0]);
+console.log(jsonPracownicy.pracownicy[1]);
+console.log(jsonPracownicy.pracownicy[2]);
+
+for (var i = 0; i < jsonPracownicy.pracownicy; i++){
+    console.log(json.pracownicy.lastName);
+}
